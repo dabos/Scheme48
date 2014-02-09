08ce3ce16d2eca18b2f4a4cffd4ec97a6f0aedba
alexbos <bostan.alex@gmail.com>
2014-02-09 17:51:44 +0000

    first commit

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,18 @@
 -- | Main entry point to the application.
 module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
 
 -- | The main entry point.
 main :: IO ()
-main = do
-    putStrLn "Welcome to FP Haskell Center!"
-    putStrLn "Have a good day!"
+main = do line <- getLine
+          putStrLn (line)
